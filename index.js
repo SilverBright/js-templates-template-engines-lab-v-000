8f4a01541decedd1ec9353840b90c5b01b301214
Barbara Washington <barbara.m.washington@gmail.com>
2019-01-29 22:22:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ function createPost() {
   const pageTemplate = _.template(document.getElementById("page-template").innerHTML);
   const postTemplate = _.template(document.getElementById("post-template").innerHTML);
   const commentsTemplate = _.template(document.getElementById("comments-template").innerHTML);
-  
+  const postTitle = document.getElementById("postTitle").value;
+  const postAuthor = document.getElementById("postAuthor").value;
+  const post = document.getElementById("postBody").value;
   
 }
 
